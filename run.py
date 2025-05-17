2fe6f4f80cb930a8451c238435e11f2233570fe3
Samuel García <sam.dgarcia02@gmail.com>
2025-05-18 05:18:33 +1200

    Update run.py
    Signed-off-by: Samuel García  <sam.dgarcia02@gmail.com>

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import os
 from app import create_app
+from tools.manual_pin_change import crear_tabla_si_no_existe
+crear_tabla_si_no_existe()
 
 # Usa "development" por defecto si no se especifica APP_ENV
 env = os.getenv("APP_ENV", "development")
