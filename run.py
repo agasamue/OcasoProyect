1f0dd726180d18853c63e4e52861ea960866c913
Samuel García <sam.dgarcia02@gmail.com>
2025-05-18 06:02:41 +1200

    Update run.py
    Signed-off-by: Samuel García  <sam.dgarcia02@gmail.com>

--- run.py
+++ run.py
@@ -1,16 +1,18 @@
 import os
 from app import create_app
 from tools.manual_pin_change import crear_tabla_si_no_existe
-crear_tabla_si_no_existe()
+from app.models import db
 
-# Usa "development" por defecto si no se especifica APP_ENV
 env = os.getenv("APP_ENV", "development")
 
-# Crea la app con la configuración correspondiente
 app = create_app(env)
 
+crear_tabla_si_no_existe()
+
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    # Ejecuta en modo debug solo si no es producción
     debug_mode = env != "production"
     app.run(host="0.0.0.0", port=5000, debug=debug_mode)
 
